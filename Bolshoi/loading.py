5e74b30046b09a088c2be405f517f25dcd142fc2
Nikhil0504 <nikhilgaruda@live.com>
2022-10-17 08:55:44 -0700

    Add multiple halo dispersion script and plots

--- Bolshoi/loading.py
+++ Bolshoi/loading.py
@@ -9,3 +9,5 @@ z = np.load(f'{CACHE_PATH}/z_points.npy')
 rvir = np.load(f'{CACHE_PATH}/rvir_points.npy')
 rs = np.load(f'{CACHE_PATH}/rs_points.npy')
 arr_points = np.load(f'{CACHE_PATH}/arr_points.npy')
+
+np.random.seed(10)
