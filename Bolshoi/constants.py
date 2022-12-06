db54ea8cc4b7e630a972a411558be7073bfa030f
Nikhil0504 <nikhilgaruda@live.com>
2022-12-06 14:17:48 -0700

    new plot parameters and mass bins

--- Bolshoi/constants.py
+++ Bolshoi/constants.py
@@ -16,19 +16,19 @@ RADIUS = (RADIUS_BINS[1:] + RADIUS_BINS[:-1]) / 2.0
 
 # PLOTTING SPECS
 plt.style.use(["science"])  # type: ignore
-# plt.rcParams.update({'figure.dpi': '200'})
 plt.rcParams["text.usetex"] = True
-plt.rcParams["figure.figsize"] = (20, 20)
-plt.rcParams["xtick.major.size"] = 15
+plt.rcParams["figure.figsize"] = (5, 5)
+plt.rcParams["xtick.major.size"] = 12
 plt.rcParams["xtick.minor.size"] = 7
-plt.rcParams["ytick.major.size"] = 15
+plt.rcParams["ytick.major.size"] = 12
 plt.rcParams["ytick.minor.size"] = 7
-plt.rcParams["xtick.labelsize"] = 15
-plt.rcParams["ytick.labelsize"] = 15
-plt.rcParams["font.size"] = 25
-plt.rcParams["legend.fontsize"] = 30
-plt.rcParams['xtick.direction'] = 'inout'
-plt.rcParams['ytick.direction'] = 'inout'
+plt.rcParams["xtick.labelsize"] = 13
+plt.rcParams["ytick.labelsize"] = 13
+plt.rcParams["font.size"] = 15
+plt.rcParams["legend.fontsize"] = 12
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['figure.dpi'] = 300
 
 
 # PATHS
