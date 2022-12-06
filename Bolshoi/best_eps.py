ee249b935ad6ab8eff8b1f25b1ce6a84a0c1261b
Nikhil0504 <nikhilgaruda@live.com>
2022-12-06 14:19:59 -0700

    new eps script

--- Bolshoi/best_eps.py
+++ Bolshoi/best_eps.py
@@ -1,120 +1,176 @@
 # %%
-from loading import *
-import concurrent.futures
-
-plt.rcParams["figure.figsize"] = (10, 10)
-plt.rcParams["font.size"] = 20
-plt.rcParams["legend.fontsize"] = 20
+from functions import *
 
 # %%
-a = np.arange(9)
-total = None
-
-bins = MASS_BINS[:2]
-eps = np.arange(0.01, 0.2, 0.01)
-ses = []
-ep = []
+fi = np.genfromtxt('inds.txt')
 
 # %%
-def tst(eps):    
-    total = None
-    for ind in range(bins.size - 1):
-        m_ind = np.nonzero((data_points <= bins[ind + 1]) & (data_points > bins[ind]))[0]
-        N_samples = min([len(m_ind), 100])
-        r_ind = np.random.choice(m_ind, N_samples, replace=False)
+slices = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])
 
-        # print(f"{eps} Samples: {N_samples}")
+# %%
+nfi = fi[slices[-2]:slices[-1]]
+nfi.shape
 
-        main = r_ind.reshape(-1, 1)
-        _M = data_points[r_ind].reshape(-1, 1)
-        _c = (rvir[r_ind] / rs[r_ind]).reshape(-1, 1)
-        main = np.column_stack((main, _M, _c))
-        opts = np.array([])
+# %%
+main = np.empty((nfi.shape[0], 0))
 
-        for index in a:
-            # print(f"{np.log10(bins[ind])}-{np.log10(bins[ind + 1])} -> J: {index}")
-            for r in r_ind:
-                # print(eps, r)
-                M = data_points[r]
-                Rvir = rvir[r] / 1000
+# %%
+for i in range(9):
+    opts = np.array([])
+    for halo in nfi:
+        M = halo[1]
+        Rvir = halo[2]
+        mask = np.where(RADIUS < Rvir)
+        obs = halo[3+(i*BIN_NO):3+((i+1)*BIN_NO)]
+        obs = obs[mask]
+        # c_inv = np.diag((0.25 * obs) ** 2)
+        try:
+            c_inv = cinv(obs, 0.01)
+            optres = iminuit.minimize(
+                cost, [np.log(10)], args=(obs, c_inv, M, Rvir, "gaussian")
+            )
+            opts = np.append(opts, optres.x)
+        except np.linalg.LinAlgError:
+            # pass
+            opts = np.append(opts, np.nan)
+    
+    opts = opts.reshape(-1, 1)
+    # print(opts.shape)
 
-                X, Y, Z = x[r], y[r], z[r]
-                arr_points_2 = get_points(X, Y, Z, arr_points)
+    main = np.column_stack((main, opts))
 
-                R = compute_R2(arrays(arr_points_2, X, Y, Z, index + 1))
-                pairs, _ = np.histogram(R, bins=RADIUS_BINS)
-                total_mass = np.array(pairs) * MASS * (100 / PERCENT)
+# print(main.shape)
+main = main[~np.isnan(main).any(axis=1), :]
+print(main.shape)
 
-                if index == 1:
-                    volume = Volume(1)
-                else:
-                    volume = Volume(7.0 / 8.0)
+# %%
+meanjk = np.mean(main[:, 1:], axis=1)
+main = np.column_stack((main, meanjk))
+main.shape
 
-                obs = total_mass / volume
+# %%
+se_jk = se_jack(main[:, 1:-1], main[:, -1], 8)
+main = np.column_stack((main, se_jk))
+main.shape
 
-                mask = np.where(RADIUS < rvir[r] / 1000)
+# %%
+plt.hist(se_jk, bins=20)
+plt.xlim(0, 10)
+plt.xticks(np.arange(0, 11, 1))
+plt.xlabel('$\sigma_{jk}$')
+plt.ylabel('N')
 
+# %%
+def best_eps(fi, slices, eps, loss: str):
+    # plt.rcParams['xtick.direction'] = 'out'
+    # plt.rcParams['ytick.direction'] = 'out'
+    # fig = plt.figure(figsize=(20, 20))
+    # plt.tight_layout(w_pad=3)
+    # i = 1
+    se = np.array([])
+    for bin in range(slices.shape[0] - 1):
+        # print('bin', bin)
+        nfi = fi[slices[bin]:slices[bin+1]]
+
+        main = np.empty((nfi.shape[0], 0))
+
+        for jack in range(9):
+            # print('bin', bin, 'jack',jack)
+            opts = np.array([])
+            for halo in nfi:
+                M = halo[1]
+                Rvir = halo[2]
+
+                mask = np.where(RADIUS < Rvir)
+
+                obs = halo[3+(jack*BIN_NO):3+((jack+1)*BIN_NO)]
                 obs = obs[mask]
-                c_inv = cinv(obs, eps)
 
-                optres = iminuit.minimize(
-                    cost, [np.log(10)], args=(obs, c_inv, M, Rvir, "lorentz")
-                )
-                opts = np.append(opts, np.exp(optres.x))
+                try:
+                    c_inv = cinv(obs, eps)
+                    optres = iminuit.minimize(
+                            cost, [np.log(10)], args=(obs, c_inv, M, Rvir, loss)
+                        )
+                    # optres = scipy.optimize.minimize(
+                    #     cost, [np.log(10)], args=(obs, c_inv, M, Rvir, loss
+                    # ))
+                    opts = np.append(opts, optres.x)
+                except np.linalg.LinAlgError:
+                    # pass
+                    opts = np.append(opts, np.nan)
 
             opts = opts.reshape(-1, 1)
-            # print(opts.shape)
             main = np.column_stack((main, opts))
-            opts = np.array([])
-            # print(eps, main.shape)
-        
-            
-        m_c = np.mean(main[:, 4:], axis=1)
-        main = np.column_stack((main, m_c))
-        # print(eps, main.shape)
-        
-        if total is None:
-            total = main
-            # print(eps, total.shape)
-        else:
-            total = np.vstack((total, main))  # type: ignore
-            # print(eps, total.shape)
-    
-    jacks = total[:, 4:-1] # type: ignore
-    meanjk = total[:, -1] # type: ignore
-    se_jk = se_jack(jacks, meanjk, jacks.shape[1])
-    # ses = np.append(ses, np.mean(se_jk))
-    # print(eps, np.mean(se_jk))
-    return eps, np.mean(se_jk)
+
+        # drop nan (det 0)
+        main = main[~np.isnan(main).any(axis=1), :]
+        meanjk = np.mean(main[:, 1:], axis=1)
+        main = np.column_stack((main, meanjk))
+
+        se_jk = se_jack(main[:, 1:-1], main[:, -1], 8)
+        main = np.column_stack((main, se_jk))
+        se = np.append(se, np.mean(se_jk))
+
+        # plt.subplot(5, 5, i)
+        # i += 1
+        # plt.hist(se_jk, bins=20)
+        # plt.xlim(0, 10)
+        # plt.xticks(np.arange(0, 11, 1))
+        # plt.xlabel('$\sigma_{jk}$')
+        # plt.ylabel('N')
+        # plt.title(bin + 1)
+    # plt.show()
+    # plt.savefig(f'{eps}.png')
+    return se
 
 # %%
-def update_ses(s):
-    ep.append(s[0])
-    ses.append(s[1])
+dummy = np.arange(0.01, 0.26, 0.01)
+len(dummy)
 
 # %%
-# executor = concurrent.futures.ProcessPoolExecutor(10)
-# futures = [executor.submit(tst, ep) for ep in eps]
-# concurrent.futures.wait(futures)
-
-def start_processing(eps):
-    with concurrent.futures.ProcessPoolExecutor(9) as executor:
-        future_proc = {executor.submit(tst, ep) for ep in eps}
-        for future in concurrent.futures.as_completed(future_proc):
-            update_ses(future.result())
+allse = np.genfromtxt('test_eps.out')
+# allse = np.array([])
+for e in dummy:
+    # print('eps', e)
+    se = best_eps(fi, slices, e, "lorentz")
+    if not allse.size > 0:
+        allse = se.reshape(-1, 1)
+    else:
+        se = se.reshape(-1, 1)
+        allse = np.column_stack((allse, se))
+    np.savetxt('test_eps.out', allse)
+allse.shape
 
 # %%
-start_processing(eps)
+allse.shape
 
 # %%
-# plt.plot(eps, ses)
-# print('after parallel')
-# for e, s in zip(ep, ses):
-#     print(f'{e} {s}')
-mi = np.argmin(ses)
-plt.scatter(ep, ses)
-plt.axvline(ep[mi], label=r'$\sum \frac{(\rho_{mod} - \rho_{data})^2}{{(\epsilon \rho_{data})}^2}$')
-plt.ylabel(r'$< \sigma_{jk} >$')
-plt.xlabel(r'$\epsilon$')
-plt.title(f'Min $\\epsilon$: {ep[mi]}')
-plt.savefig('eps_lorentz_low.png')
+fi2 = allse
+
+# %%
+fi2 = np.genfromtxt('test_eps.out')
+fi2.shape
+
+# %%
+avgm = (MASS_BINS[1:] + MASS_BINS[:-1])/2
+
+# %%
+# dummy = np.arange(0.01, 0.26, 0.01)
+fig, _ = plt.subplots(figsize=(20, 20))
+fig.tight_layout()
+for i in range(10):
+    mi = np.argmin(fi2[i, :])
+    plt.subplot(5, 5, i+1)
+    plt.scatter(dummy[0:10], fi2[i, :], label=f'{np.log10(MASS_BINS[i])} - {np.log10(MASS_BINS[i+1])}')
+    plt.axvline(dummy[mi], color='r', label=f'Min $\epsilon$ = {dummy[mi]:.2f}')
+    plt.ylabel(r'$< \sigma_{jk} >$')
+    plt.xlabel(r'$\epsilon$')
+    # plt.ylim(0, 2.5)
+    plt.xticks(dummy[::4])
+    plt.legend()
+plt.show()
+
+# %%
+
+
+
