43e798ca640bd04af6d1fa51fe5735545cf24223
Nikhil0504 <nikhilgaruda@live.com>
2022-12-17 00:42:21 -0700

    add new functions for getting the points from the array and computing the distance and it's volume and a cinv function

--- Bolshoi/functions.py
+++ Bolshoi/functions.py
@@ -1,7 +1,40 @@
-from imports import np
+from constants import RADIUS_BINS
+from imports import np, njit, jit
 
 
-def arrays(array: np.ndarray, X: int, Y: int, Z: int, i: int) -> np.ndarray: # type: ignore
+@njit(parallel=True)
+def get_points(x, y, z, arr_points) -> np.ndarray:
+    return arr_points[
+        (arr_points[:, 0] < x + 10)
+        & (x - 10 < arr_points[:, 0])
+        & (arr_points[:, 1] < y + 10)
+        & (y - 10 < arr_points[:, 1])
+        & (arr_points[:, 2] < z + 10)
+        & (z - 10 < arr_points[:, 2])
+    ]
+
+@njit(parallel=True, fastmath=True)
+def compute_R(x, y, z, arr, ind):
+    Arrays = arrays(get_points(x, y, z, arr), x, y, z, ind)
+    return np.sqrt(
+        (Arrays[:, 0]) ** 2.0 + (Arrays[:, 1]) ** 2.0 + (Arrays[:, 2]) ** 2.0
+    )
+
+@jit(fastmath=True)
+def Volume(factor, bn=25):
+    volume = []
+    for i in range(bn):
+        vol = factor * 4.0 / 3.0 * np.pi * (RADIUS_BINS[i + 1] ** 3 - RADIUS_BINS[i] ** 3)
+        volume.append(vol)
+    return np.array(volume)
+
+@jit()
+def cinv(obs, epsilon):
+    c = np.diag((epsilon * obs) ** 2)
+    return np.linalg.inv(c)
+
+
+def arrays(array: np.ndarray, X: int, Y: int, Z: int, i: int) -> np.ndarray:  # type: ignore
     array1 = array - [X, Y, Z]
     if i == 1:
         return array1
