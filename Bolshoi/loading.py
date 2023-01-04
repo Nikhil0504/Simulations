5412a15d04ba6fde3a73d3193744f4c3d0b5644b
Nikhil0504 <nikhilgaruda@live.com>
2023-01-03 22:59:50 -0700

    make loading speeds 5000x faster using `mmap`

--- Bolshoi/loading.py
+++ Bolshoi/loading.py
@@ -5,14 +5,14 @@ from functions import *
 from imports import *
 
 st = time.time()
-mvir = np.load(f"{CACHE_PATH}/halofunc_points.npy")
-x = np.load(f"{CACHE_PATH}/x_points.npy")
-y = np.load(f"{CACHE_PATH}/y_points.npy")
-z = np.load(f"{CACHE_PATH}/z_points.npy")
-rvir = np.load(f"{CACHE_PATH}/rvir_points.npy")
-rs = np.load(f"{CACHE_PATH}/rs_points.npy")
-arr_points = np.load(f"{CACHE_PATH}/arr_points.npy")
+mvir = np.load(f"{CACHE_PATH}/halofunc_points.npy", mmap_mode='r')
+x = np.load(f"{CACHE_PATH}/x_points.npy", mmap_mode='r')
+y = np.load(f"{CACHE_PATH}/y_points.npy", mmap_mode='r')
+z = np.load(f"{CACHE_PATH}/z_points.npy", mmap_mode='r')
+rvir = np.load(f"{CACHE_PATH}/rvir_points.npy", mmap_mode='r')
+rs = np.load(f"{CACHE_PATH}/rs_points.npy", mmap_mode='r')
+arr_points = np.load(f"{CACHE_PATH}/arr_points.npy", mmap_mode='r')
 print("Loaded Cache Files")
-print(f"Time taken to load cache files: {time.time()-st:2.2f}")
+print(f"Time taken to load cache files: {time.time()-st} s")
 
 np.random.seed(10)
