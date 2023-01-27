531224e88455164389b8f81a12566cf91d4c3108
Nikhil0504 <nikhilgaruda@live.com>
2023-01-27 07:53:59 -0700

    Change Cache File Variables to avoid conflicts

--- Bolshoi/loading.py
+++ Bolshoi/loading.py
@@ -5,13 +5,15 @@ from functions import *
 from imports import *
 
 st = time.time()
-mvir = np.load(f"{CACHE_PATH}/halofunc_points.npy", mmap_mode='r')
-x = np.load(f"{CACHE_PATH}/x_points.npy", mmap_mode='r')
-y = np.load(f"{CACHE_PATH}/y_points.npy", mmap_mode='r')
-z = np.load(f"{CACHE_PATH}/z_points.npy", mmap_mode='r')
-rvir = np.load(f"{CACHE_PATH}/rvir_points.npy", mmap_mode='r')
-rs = np.load(f"{CACHE_PATH}/rs_points.npy", mmap_mode='r')
-arr_points = np.load(f"{CACHE_PATH}/arr_points.npy", mmap_mode='r')
+
+MVIR = np.load(f"{CACHE_PATH}/halofunc_points.npy", mmap_mode='r')
+X = np.load(f"{CACHE_PATH}/x_points.npy", mmap_mode='r')
+Y = np.load(f"{CACHE_PATH}/y_points.npy", mmap_mode='r')
+Z = np.load(f"{CACHE_PATH}/z_points.npy", mmap_mode='r')
+RVIR = np.load(f"{CACHE_PATH}/rvir_points.npy", mmap_mode='r')
+RS = np.load(f"{CACHE_PATH}/rs_points.npy", mmap_mode='r')
+ARR_POINTS = np.load(f"{CACHE_PATH}/arr_points.npy", mmap_mode='r')
+
 print("Loaded Cache Files")
 print(f"Time taken to load cache files: {time.time()-st} s")
 
