9a9a5101e364567e5ce3f40114d8bab8ee2e3fac
Nikhil0504 <nikhilgaruda@live.com>
2023-01-27 07:54:34 -0700

    Add scipy `basinhopping` for global optimisation

--- Bolshoi/halos.py
+++ Bolshoi/halos.py
@@ -7,14 +7,14 @@ from imports import iminuit, jit, np
 
 class Halo:
 
-    def __init__(self, hid: int, mvir, x, y, z, rvir, rs):
-        self.hid = hid
-        self.mvir = mvir[hid]
-        self.x = x[hid]
-        self.y = y[hid]
-        self.z = z[hid]
-        self.rvir = rvir[hid] / 1000
-        self.rs = rs[hid] / 1000
+    def __init__(self, hid: float, mvir, x, y, z, rvir, rs):
+        self.hid = int(hid)
+        self.mvir = mvir[self.hid]
+        self.x = x[self.hid]
+        self.y = y[self.hid]
+        self.z = z[self.hid]
+        self.rvir = rvir[self.hid] / 1000
+        self.rs = rs[self.hid] / 1000
         self.cvir = self.rvir / self.rs
 
     @property
@@ -51,18 +51,42 @@ class Halo:
         radii, rhos = rho_r(Rs, self.Mvir, self.Rvir, mask)
         return radii, rhos
 
-    def minimise_cost(self, Den=[], eps=0.25, cost_func="gaussian"):
+    def minimise_cost(self, Den=[], eps=0.25, cost_func="gaussian", lib='iminuit'):
         den = Den
 
         mask = np.where((RADIUS < self.Rvir) & (den > 0))
         den = den[mask]
 
-        # optres = so.minimize(
-        #     cost, [np.log(10)], args=(den, eps, self.Mvir, self.Rvir, mask, cost_func), method='Nelder-Mead', bounds=[(np.log(1e-50), np.log(50))]
-        # )
-        optres = iminuit.minimize(cost, [np.log(10)],
-                                  args=(den, eps, self.Mvir, self.Rvir, mask,
-                                        cost_func),
-                                  method='simplex',
-                                  bounds=(np.log(1e-50), np.log(50)))
-        return optres.x
+        
+        if lib == 'iminuit':
+            optres = iminuit.minimize(cost, [np.log(5)],
+                                    args=(den, eps, self.Mvir, self.Rvir, mask,
+                                            cost_func),
+                                    method='simplex',
+                                    bounds=(np.log(1e-2), np.log(50)),
+                                    tol=1e-4,
+                                    options={'stra': 2, 'maxfun': 500})
+            return optres.x
+        
+        elif lib == 'scipy':
+            # optres = so.minimize(
+            #     cost, np.log(5), 
+            #     args=(den, eps, self.Mvir, self.Rvir, mask, cost_func), 
+            #     method='Nelder-Mead', bounds=[(np.log(1e-2), np.log(50))],
+            #     tol=1e-4,
+            #     options={'maxiter': 500}
+            # )
+
+            optres = so.basinhopping(cost, np.log(5),stepsize=1,
+            minimizer_kwargs={"method": "Nelder-Mead", 
+                              "args": (den, eps, self.Mvir, self.Rvir, mask, cost_func),
+                              "bounds": [(np.log(1e-2), np.log(50))]})
+
+            # optres = so.shgo(cost, bounds=[(np.log(1e-5), np.log(50))],
+            #                 args=(den, eps, self.Mvir, self.Rvir, mask, cost_func),
+            #                 sampling_method='sobol', 
+            #                 minimizer_kwargs={'method': 'Nelder-Mead'})
+
+            return optres.x
+        
+        return np.inf
