8d7f71584acdf19fad36d82873e281271028cc71
Nikhil0504 <nikhilgaruda@live.com>
2023-01-27 07:55:02 -0700

    new eps scripts

--- Bolshoi/eps.py
+++ Bolshoi/eps.py
@@ -1,9 +1,6 @@
 import sys
-from typing import Iterable, Union
-
-from constants import MASS_BINS, RADIUS
-from functions import cost, se_jack
-from imports import iminuit, np, plt
+from loading import *
+from halos import Halo
 
 plt.style.use(["science", "scatter"])
 
@@ -12,59 +9,22 @@ def Eps(
     inds: np.ndarray,
     ep: float,
     func: str = "gaussian",
-    bounds: Union[None, Iterable[float]] = None,
+    lib: str = "scipy"
 ) -> np.ndarray:
-    """Gets the best-fits using the minimiser for a particular
-    epsilion value.
-
-    Parameters
-    ----------
-    inds : np.ndarray
-        The indices file containing the ID, Mvir, Rvir, Densities
-        along with Jack-knife Densities.
-    ep : float
-        The epsilion value for the cost function.
-    func : str, optional
-        The cost function, by default "gaussian"
-    bounds : Union[None, Iterable[float]], optional
-        The bounds for the minimiser, by default None
-
-    Returns
-    -------
-    np.ndarray
-        An array of the best fit ln(cvir). Each column is a
-        jack-knife with the first column being the full halo.
-    """
     main = np.zeros(0)
 
-    for halo in inds:
-        M = halo[1]
-        Rvir = halo[2]
+    for id, halo in enumerate(inds):
+        print(f'{func} {ep} {id}')
+        h = Halo(int(halo[0]), MVIR, X, Y, Z, RVIR, RS)
 
         opts = np.zeros(0)
 
         for jack in range(9):
             density = halo[3 + (jack * 25):3 + ((jack + 1) * 25)]
-            mask = np.where((RADIUS < Rvir) & (density > 0))
-            density = density[mask]
 
-            # optres = so.minimize(
-            #     cost,
-            #     np.log(10),
-            #     args=(density, ep, M, Rvir, mask, func),
-            #     bounds=bounds,
-            #     method='Nelder-Mead'
-            # )
+            optres = h.minimise_cost(density, ep, func, lib)
 
-            optres = iminuit.minimize(
-                cost,
-                np.log(10),
-                args=(density, ep, M, Rvir, mask, func),
-                bounds=bounds,
-                method="simplex",
-            )
-
-            opts = np.append(opts, optres.x)
+            opts = np.append(opts, optres)
 
         if main.size == 0:
             main = opts
@@ -79,26 +39,26 @@ epss = np.arange(0.01, 0.26, 0.01)
 
 mean_se = np.array([])
 
-func = sys.argv[1]
+func = sys.argv[-1]
 print(func)
 
 for ep in epss:
-    main = Eps(inds, ep, func, bounds=(np.log(1e-50), np.log(50)))
+    main = Eps(inds, ep, func, 'scipy')
     temp = se_jack(main[:, 1:], np.mean(main[:, 1:], axis=1), 8)
 
     slices = np.array(
         [0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])
 
-    y = np.array([])
+    medians = np.array([])
 
     for i in range(10):
         new = temp[slices[i]:slices[i + 1]]
-        y = np.append(y, np.mean(new))
+        medians = np.append(medians, np.median(new))
 
     if mean_se.size == 0:
-        mean_se = y
+        mean_se = medians
     else:
-        mean_se = np.vstack((mean_se, y))
+        mean_se = np.vstack((mean_se, medians))
 
     print(f"{func} {ep} done.")
 
@@ -121,7 +81,7 @@ for i in range(10):
 
     plt.ylim(np.mean(mean_se[:, i]) - 0.02, np.mean(mean_se[:, i]) + 0.02)
     plt.xlabel(r"$\epsilon$")
-    plt.ylabel(r"$<\sigma_{jk}>$")
+    plt.ylabel(r"$Med(\sigma_{jk})$")
 
     plt.legend(prop={"size": 8})
 
--- Bolshoi/eps_25_plots.py
+++ Bolshoi/eps_25_plots.py
@@ -1,72 +1,33 @@
-import sys
-from typing import Iterable, Union
+# %%
+from loading import *
+from halos import Halo
 
-import scipy.optimize as so
+# %%
+inds = np.loadtxt("out/inds.txt")
+slices = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])[4:]
+MASS_BINS = MASS_BINS[4:]
 
-from constants import MASS_BINS, RADIUS
-from functions import cost, se_jack
-from imports import iminuit, np, plt
-
-plt.style.use(["science"])
-
-
+# %%
 def Eps(
     inds: np.ndarray,
     ep: float,
     func: str = "gaussian",
-    bounds: Union[None, Iterable[float]] = None,
+    lib: str = "iminuit"
 ) -> np.ndarray:
-    """Gets the best-fits using the minimiser for a particular
-    epsilion value.
-
-    Parameters
-    ----------
-    inds : np.ndarray
-        The indices file containing the ID, Mvir, Rvir, Densities
-        along with Jack-knife Densities.
-    ep : float
-        The epsilion value for the cost function.
-    func : str, optional
-        The cost function, by default "gaussian"
-    bounds : Union[None, Iterable[float]], optional
-        The bounds for the minimiser, by default None
-
-    Returns
-    -------
-    np.ndarray
-        An array of the best fit ln(cvir). Each column is a
-        jack-knife with the first column being the full halo.
-    """
     main = np.zeros(0)
 
-    for halo in inds:
-        M = halo[1]
-        Rvir = halo[2]
+    for id, halo in enumerate(inds):
+        print(f'{lib} {id}')
+        h = Halo(int(halo[0]), MVIR, X, Y, Z, RVIR, RS)
 
         opts = np.zeros(0)
 
         for jack in range(9):
             density = halo[3 + (jack * 25):3 + ((jack + 1) * 25)]
-            mask = np.where((RADIUS < Rvir) & (density > 0))
-            density = density[mask]
 
-            # optres = so.minimize(
-            #     cost,
-            #     np.log(10),
-            #     args=(density, ep, M, Rvir, mask, func),
-            #     bounds=bounds,
-            #     method='Nelder-Mead'
-            # )
+            optres = h.minimise_cost(density, ep, func, lib)
 
-            optres = iminuit.minimize(
-                cost,
-                np.log(10),
-                args=(density, ep, M, Rvir, mask, func),
-                bounds=bounds,
-                method="simplex",
-            )
-
-            opts = np.append(opts, optres.x)
+            opts = np.append(opts, optres)
 
         if main.size == 0:
             main = opts
@@ -75,222 +36,137 @@ def Eps(
 
     return main
 
+# %%
+main = Eps(inds, 0.10, 'gaussian', "scipy")
+main2 = Eps(inds, 0.10, 'lorentz', "scipy")
+main3 = Eps(inds, 0.10, 'abs', "scipy")
 
-inds = np.loadtxt("out/inds.txt")
+# %%
+temp = se_jack(main[:, 1:], np.mean(main[:, 1:], axis=1), 8)
+temp2 = se_jack(main2[:, 1:], np.mean(main2[:, 1:], axis=1), 8)
+temp3 = se_jack(main3[:, 1:], np.mean(main3[:, 1:], axis=1), 8)
 
-slices = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])
-
-func = sys.argv[1]
-print(func)
-
-imain = Eps(inds, 0.25, "gaussian", bounds=(np.log(1e-50), np.log(50)))
-imain2 = Eps(inds, 0.25, "lorentz", bounds=(np.log(1e-50), np.log(50)))
-imain3 = Eps(inds, 0.25, "abs", bounds=(np.log(1e-50), np.log(50)))
+# %%
+imain = Eps(inds, 0.10, 'gaussian', 'iminuit')
+imain2 = Eps(inds, 0.10, 'lorentz', 'iminuit')
+imain3 = Eps(inds, 0.10, 'abs', 'iminuit')
 
 itemp = se_jack(imain[:, 1:], np.mean(imain[:, 1:], axis=1), 8)
 itemp2 = se_jack(imain2[:, 1:], np.mean(imain2[:, 1:], axis=1), 8)
 itemp3 = se_jack(imain3[:, 1:], np.mean(imain3[:, 1:], axis=1), 8)
 
-x, iy, iy2, iy3, iy4, iy5, iy6 = (np.zeros(0) for _ in range(7))
-
-iy7, iy8, iy9, z, z2, z3 = (np.zeros(0) for _ in range(6))
+# %%
+X, Y, y2, y3, y4, y5, y6 = (np.zeros(0) for _ in range(7))
 
 for i in range(slices.shape[0] - 1):
-    iy = np.append(iy, np.mean(itemp[slices[i]:slices[i + 1]]))
-    iy3 = np.append(iy3, np.mean(itemp2[slices[i]:slices[i + 1]]))
-    iy5 = np.append(iy5, np.mean(itemp3[slices[i]:slices[i + 1]]))
+    Y = np.append(Y, np.median(temp[slices[i]: slices[i+1]]))
+    y3 = np.append(y3, np.median(temp2[slices[i]: slices[i+1]]))
+    y5 = np.append(y5, np.median(temp3[slices[i]: slices[i+1]]))
 
-    iy2 = np.append(iy2, np.std(imain[slices[i]:slices[i + 1], 0]))
-    iy4 = np.append(iy4, np.std(imain2[slices[i]:slices[i + 1], 0]))
-    iy6 = np.append(iy6, np.std(imain3[slices[i]:slices[i + 1], 0]))
+    y2 = np.append(y2, np.std(main[slices[i]: slices[i + 1], 0]))
+    y4 = np.append(y4, np.std(main2[slices[i]: slices[i + 1], 0]))
+    y6 = np.append(y6, np.std(main3[slices[i]: slices[i + 1], 0]))
 
-    iy7 = np.append(iy7, np.std(itemp[slices[i]:slices[i + 1]]))
-    iy8 = np.append(iy8, np.std(itemp2[slices[i]:slices[i + 1]]))
-    iy9 = np.append(iy9, np.std(itemp3[slices[i]:slices[i + 1]]))
+# %%
+X, iy, iy2, iy3, iy4, iy5, iy6 = (np.zeros(0) for _ in range(7))
 
-    z = np.append(z, np.mean(imain[slices[i]:slices[i + 1], 0]))
-    z2 = np.append(z2, np.mean(imain2[slices[i]:slices[i + 1], 0]))
-    z3 = np.append(z3, np.mean(imain3[slices[i]:slices[i + 1], 0]))
+for i in range(slices.shape[0] - 1):
+    iy = np.append(iy, np.median(itemp[slices[i]: slices[i+1]]))
+    iy3 = np.append(iy3, np.median(itemp2[slices[i]: slices[i+1]]))
+    iy5 = np.append(iy5, np.median(itemp3[slices[i]: slices[i+1]]))
 
-    x = np.append(x, np.mean([MASS_BINS[i], MASS_BINS[i + 1]]))
+    iy2 = np.append(iy2, np.std(imain[slices[i]: slices[i + 1], 0]))
+    iy4 = np.append(iy4, np.std(imain2[slices[i]: slices[i + 1], 0]))
+    iy6 = np.append(iy6, np.std(imain3[slices[i]: slices[i + 1], 0]))
 
-##########################
+    X = np.append(X, np.mean([MASS_BINS[i], MASS_BINS[i + 1]]))
+
+# %%
+plt.style.use(['science'])
+
 fig = plt.figure(figsize=(5, 5), dpi=150)
 
-plt.plot(
-    x,
-    iy,
-    "#008BF8",
-    linestyle="--",
-    linewidth=1.5,
-    label="_Jack-knife Gaussian Iminuit",
-)
-plt.plot(
-    x,
-    iy2,
-    "#DC0073",
-    linestyle="--",
-    linewidth=1.5,
-    label="_Intrinsic Gaussian Iminuit",
-)
+plt.plot(X, Y, 'g--', linewidth=1.5, label='_Jack-knife Gaussian')
+plt.plot(X, y2, 'r--', linewidth=1.5, label='_Intrinsic Gaussian')
 
-plt.plot(x,
-         iy3,
-         "#008BF8",
-         linestyle=":",
-         linewidth=1.5,
-         label="_Jack-knife Lorentz Iminuit")
-plt.plot(x,
-         iy4,
-         "#DC0073",
-         linestyle=":",
-         linewidth=1.5,
-         label="_Intrinsic Lorentz Iminuit")
+plt.plot(X, y3, 'g', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz')
+plt.plot(X, y4, 'r', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz')
 
-plt.plot(
-    x,
-    iy5,
-    "#008BF8",
-    linestyle="-",
-    linewidth=1.5,
-    label="_Jack-knife Absolute Iminuit",
-)
-plt.plot(x,
-         iy6,
-         "#DC0073",
-         linestyle="-",
-         linewidth=1.5,
-         label="_Intrinsic Absolute Iminuit")
+plt.plot(X, iy, '#008BF8', linestyle='--', linewidth=1.5, label='_Jack-knife Gaussian Iminuit')
+plt.plot(X, iy2, '#DC0073', linestyle='--', linewidth=1.5, label='_Intrinsic Gaussian Iminuit')
 
-plt.plot([], [], "--", color="#1E152A", linewidth=1.5, label="Gaussian")
-plt.plot([], [], ":", color="#1E152A", linewidth=1.5, label="Lorentz")
-plt.plot([], [], "-", color="#1E152A", linewidth=1.5, label="Absolute")
+plt.plot(X, iy3, '#008BF8', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz Iminuit')
+plt.plot(X, iy4, '#DC0073', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz Iminuit')
 
-plt.plot([], [], "#008BF8", linewidth=1.5, label="Jack-knife")
-plt.plot([], [], "#DC0073", linewidth=1.5, label="Intrinsic")
+plt.plot(X, y5, 'g', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute')
+plt.plot(X, y6, 'r-', linewidth=1.5, label='_Intrinsic Absolute')
 
-plt.title(f"{func.capitalize()} Loss Function, Fractional Err: 25\%")
+plt.plot(X, iy5, '#008BF8', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute Iminuit')
+plt.plot(X, iy6, '#DC0073', linestyle='-', linewidth=1.5, label='_Intrinsic Absolute Iminuit')
 
-plt.ylabel(r"$<\sigma_{\mathrm{jk}}> \mathrm{or} \ \sigma(c)$")
-plt.xlabel("Mean of the bin edges")
+plt.plot([], [], '--', color='#1E152A', linewidth=1.5, label='Gaussian')
+plt.plot([], [], ':', color='#1E152A', linewidth=1.5, label='Lorentz')
+plt.plot([], [], '-', color='#1E152A', linewidth=1.5, label='Absolute')
+plt.plot([], [], '#008BF8', linewidth=1.5, label='Jack-knife')
+plt.plot([], [], '#DC0073', linewidth=1.5, label='Intrinsic')
 
-plt.xscale("log")
+# plt.ylabel(r'$<\sigma_{\mathrm{jk}}> \mathrm{or} \ \sigma(c)$')
+plt.ylabel(r'$Med(\sigma_{\mathrm{jk}}) \ \mathrm{or} \ \sigma(c)$')
+plt.xlabel(r'$M_{\rm{vir}}$')
+plt.xscale('log')
 plt.legend()
-
-plt.savefig(f"figures/test_{func}1.png")
+# plt.show()
+plt.savefig('1.png')
 plt.clf()
 
-##########################
-fig = plt.figure(figsize=(5, 5), dpi=150)
+# %%
+plt.figure(figsize=(20,16), dpi=150)
+# plt.tight_layout()
+for i in range(slices.shape[0] - 1):
+    print(i)
+    plt.subplot(5, 5, i+1)
+    # plt.title(f'{ep * 100} \%')
+    new = itemp2[slices[i]: slices[i + 1]]
+    new2 = temp2[slices[i]: slices[i + 1]]
 
-plt.plot(
-    x,
-    iy7,
-    "#008BF8",
-    linestyle="--",
-    linewidth=1.5,
-    label="_Jack-knife Gaussian Iminuit",
-)
-plt.plot(x,
-         iy8,
-         "#008BF8",
-         linestyle=":",
-         linewidth=1.5,
-         label="_Jack-knife Lorentz Iminuit")
-plt.plot(
-    x,
-    iy9,
-    "#008BF8",
-    linestyle="-",
-    linewidth=1.5,
-    label="_Jack-knife Absolute Iminuit",
-)
 
-plt.plot([], [], "--", color="#1E152A", linewidth=1.5, label="Gaussian")
-plt.plot([], [], ":", color="#1E152A", linewidth=1.5, label="Lorentz")
-plt.plot([], [], "-", color="#1E152A", linewidth=1.5, label="Absolute")
+    tempd = y3/iy3
 
-plt.plot([], [], "#008BF8", linewidth=1.5, label="Jack-knife")
+    plt.hist(new, facecolor=(0.86, 0, 0.45, 0.7), hatch='//', histtype='stepfilled', edgecolor=(0.12, 0.08, 0.16, 0.8), bins='fd', zorder=10)
+    plt.hist(new2, facecolor=(0.44, 0.68, 0.43, 0.65), hatch='.', histtype='stepfilled', edgecolor=(0.12, 0.08, 0.16, 0.8), bins='fd')
 
-plt.title(f"{func.capitalize()} Loss Function, Fractional Err: 25\%")
+    if i == 0:
+        plt.hist([], facecolor=(0.86, 0, 0.45, 0.7), hatch='//', histtype='stepfilled', edgecolor=(0.12, 0.08, 0.16, 0.8), label='Iminuit')
+        plt.hist([], facecolor=(0.44, 0.68, 0.43, 0.65), hatch='..', histtype='stepfilled', edgecolor=(0.12, 0.08, 0.16, 0.8), label='Scipy')
+    
+    plt.plot([], color='black', label=f'\% Disagreement: {tempd[i]:1.3f}')
 
-plt.ylabel(r"$\sigma(\sigma_{jk})$")
-plt.xlabel("Mean of the bin edges")
-
-plt.xscale("log")
-plt.legend()
-
-plt.savefig(f"figures/test_{func}2.png")
+    plt.legend(prop={'size': 8})
+plt.savefig('2.png')
 plt.clf()
 
-##########################
-fig = plt.figure(figsize=(5, 5), dpi=150)
+# %%
+aaah = itemp2[slices[0]: slices[1]]
+np.where(aaah > 1)
+# inds[slices[2]: slices[3]][161][0], np.log10(inds[slices[2]: slices[3]][161][1])
 
-plt.plot(
-    x,
-    iy / z,
-    "#008BF8",
-    linestyle="--",
-    linewidth=1.5,
-    label="_Jack-knife Gaussian Iminuit",
-)
-plt.plot(
-    x,
-    iy2 / z,
-    "#DC0073",
-    linestyle="--",
-    linewidth=1.5,
-    label="_Intrinsic Gaussian Iminuit",
-)
+# %%
+plt.style.use(['science', 'scatter'])
 
-plt.plot(
-    x,
-    iy3 / z2,
-    "#008BF8",
-    linestyle=":",
-    linewidth=1.5,
-    label="_Jack-knife Lorentz Iminuit",
-)
-plt.plot(
-    x,
-    iy4 / z2,
-    "#DC0073",
-    linestyle=":",
-    linewidth=1.5,
-    label="_Intrinsic Lorentz Iminuit",
-)
+fig = plt.figure(figsize=(5, 5), dpi=300)
 
-plt.plot(
-    x,
-    iy5 / z2,
-    "#008BF8",
-    linestyle="-",
-    linewidth=1.5,
-    label="_Jack-knife Absolute Iminuit",
-)
-plt.plot(
-    x,
-    iy6 / z2,
-    "#DC0073",
-    linestyle="-",
-    linewidth=1.5,
-    label="_Intrinsic Absolute Iminuit",
-)
+plt.plot(X, Y/iy, label='Gaussian')
+plt.plot(X, y3/iy3, label='Lorentz')
+plt.plot(X, y5/iy5, label='Absolute')
 
-plt.plot([], [], "--", color="#1E152A", linewidth=1.5, label="Gaussian")
-plt.plot([], [], ":", color="#1E152A", linewidth=1.5, label="Lorentz")
-plt.plot([], [], "-", color="#1E152A", linewidth=1.5, label="Absolute")
+plt.xlabel('Mass of bin edges')
+plt.ylabel('$Med(\sigma_{\mathrm{jk, scipy}}) - Med(\sigma_{\mathrm{jk, iminuit}}) $')
 
-plt.plot([], [], "#008BF8", linewidth=1.5, label="Jack-knife")
-plt.plot([], [], "#DC0073", linewidth=1.5, label="Intrinsic")
+plt.xscale('log')
 
-plt.title(f"{func.capitalize()} Loss Function, Fractional Err: 25\%")
+plt.ylim((0.95, 1.06))
 
-plt.ylabel(r"Fractional Dispersion")
-plt.xlabel("Mean of the bin edges")
-
-plt.xscale("log")
 plt.legend()
-
-plt.savefig(f"figures/test_{func}3.png")
+# plt.show()
+plt.savefig('3.png')
 plt.clf()
