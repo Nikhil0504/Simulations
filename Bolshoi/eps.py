826fa51ec45399c7e17aad9d6e6d438bf78eefa0
Nikhil0504 <nikhilgaruda@live.com>
2023-03-03 13:04:33 -0700

    changes

--- Bolshoi/eps.py
+++ Bolshoi/eps.py
@@ -46,46 +46,55 @@ def Eps_Parallel_Helper(halo, id, ep, func, lib):
 inds = np.loadtxt("out/inds.txt")
 epss = np.arange(0.01, 0.26, 0.01)
 
-mean_se = np.array([])
+mad_se = np.array([])
 
 for ep in epss:
     main = Eps_Parallel(inds, ep)
     temp = se_jack(main[:, 1:], np.mean(main[:, 1:], axis=1), 8)
 
-    slices = np.array(
-        [0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])
+    # slices = np.array(
+        # [0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])
+    slices = np.array([0, 708, 1130, 1332, 1429, 1478, 1501])
 
     medians = np.array([])
 
-    for i in range(10):
+    for i in range(6):
         new = temp[slices[i]:slices[i + 1]]
-        medians = np.append(medians, 1.4826 * mad(new))
+        med_c = np.median(new)
+        sigma = 1.4826 * mad(new)
+        temp2 = (new - med_c) / sigma
+        # medians = np.append(medians, 1.4826 * mad(new))
+        medians = np.append(medians, len(np.where(np.abs(temp2) > 3)[0]))
 
-    if mean_se.size == 0:
-        mean_se = medians
+    if mad_se.size == 0:
+        mad_se = medians
     else:
-        mean_se = np.vstack((mean_se, medians))
+        mad_se = np.vstack((mad_se, medians))
 
 
-np.savetxt(f"out/mean_se_gaussian_mad_constrains_possion.out", mean_se)
+np.savetxt(f"out/gaussian_mad_3sigma_iminuit.out", mad_se)
+# mad_se = np.loadtxt('out/gaussian_mad_3sigma_iminuit.out')
 
 ##########################
 plt.style.use(["science"])
 
+MASS_BINS = MASS_BINS[4:]
 MASS2 = (MASS_BINS[1:] + MASS_BINS[:-1]) / 2.0
 
 fig = plt.figure(figsize=(17, 5))
 
-for i in range(10):
+for i in range(6):
     plt.subplot(2, 5, i + 1)
 
-    plt.scatter(epss, mean_se[:, i], label="")
+    plt.scatter(epss, mad_se[:, i], label="")
     plt.plot([], [], color="black", label=f"MASS: {np.log10(MASS2[i]):1.2f}")
 
-    plt.ylim(np.mean(mean_se[:, i]) - 0.02, np.mean(mean_se[:, i]) + 0.02)
-    plt.xlabel(r"$\epsilon$")
-    plt.ylabel(r"$MAD(\sigma_{jk})$")
+    # plt.ylim(np.mean(mad_se[:, i]) - 0.02, np.mean(mad_se[:, i]) + 0.02)
+    if i == 5:
+        plt.xlabel(r"$\epsilon$")
+        plt.ylabel(r"$3 \sigma (Med(\sigma_{jk}))$")
 
     plt.legend(prop={"size": 8})
 
-plt.savefig(f"figures/eps_gaussian_mad_constrains_possion.jpg", dpi=150)
+# plt.savefig(f"figures/eps_gaussian_mad_constrains_possion.jpg", dpi=150)
+plt.savefig(f"test3.jpg", dpi=150)
--- Bolshoi/eps_25_plots.py
+++ Bolshoi/eps_25_plots.py
@@ -1,12 +1,15 @@
 # %%
 import multiprocessing as mp
 
+from scipy.stats import median_abs_deviation as mad
+
 from loading import *
 from halos import Halo
 
 # %%
 inds = np.loadtxt("out/inds.txt")
-slices = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])[4:]
+# slices = np.array([0, 200, 400, 600, 800, 1000, 1200, 1400, 1497, 1545, 1568])[4:]
+slices = np.array([0, 708, 1130, 1332, 1429, 1478, 1501])
 MASS_BINS = MASS_BINS[4:]
 
 # %%
@@ -45,14 +48,14 @@ def Eps_Parallel_Helper(halo, id, ep, func, lib):
     return opts
 
 # %%
-main = Eps_Parallel(inds, 0.10, 'gaussian', "scipy")
-main2 = Eps_Parallel(inds, 0.10, 'lorentz', "scipy")
-main3 = Eps_Parallel(inds, 0.10, 'abs', "scipy")
+main = Eps_Parallel(inds, 0.10, 'gaussian', "iminuit")
+# main2 = Eps_Parallel(inds, 0.10, 'lorentz', "scipy")
+# main3 = Eps_Parallel(inds, 0.10, 'abs', "scipy")
 
 # %%
 temp = se_jack(main[:, 1:], np.mean(main[:, 1:], axis=1), 8)
-temp2 = se_jack(main2[:, 1:], np.mean(main2[:, 1:], axis=1), 8)
-temp3 = se_jack(main3[:, 1:], np.mean(main3[:, 1:], axis=1), 8)
+# temp2 = se_jack(main2[:, 1:], np.mean(main2[:, 1:], axis=1), 8)
+# temp3 = se_jack(main3[:, 1:], np.mean(main3[:, 1:], axis=1), 8)
 
 # %%
 # imain = Eps(inds, 0.10, 'gaussian', 'scipy')
@@ -67,65 +70,72 @@ temp3 = se_jack(main3[:, 1:], np.mean(main3[:, 1:], axis=1), 8)
 X, Y, y2, y3, y4, y5, y6 = (np.zeros(0) for _ in range(7))
 
 for i in range(slices.shape[0] - 1):
-    Y = np.append(Y, np.median(temp[slices[i]: slices[i+1]]))
-    y3 = np.append(y3, np.median(temp2[slices[i]: slices[i+1]]))
-    y5 = np.append(y5, np.median(temp3[slices[i]: slices[i+1]]))
+    concentrations = temp[slices[i]: slices[i+1]]
+    print(str(len(concentrations)) + '\n')
+    med_c = np.median(concentrations)
+    sigma = 1.4826 * mad(concentrations)
+    temp2 = (concentrations - med_c) / sigma
+    Y = np.append(Y, len(np.where(np.abs(temp2) > 3)[0]) / len(concentrations))
+    print(Y)
+    
+    # y3 = np.append(y3, np.median(temp2[slices[i]: slices[i+1]]))
+    # y5 = np.append(y5, np.median(temp3[slices[i]: slices[i+1]]))
 
-    y2 = np.append(y2, np.std(main[slices[i]: slices[i + 1], 0]))
-    y4 = np.append(y4, np.std(main2[slices[i]: slices[i + 1], 0]))
-    y6 = np.append(y6, np.std(main3[slices[i]: slices[i + 1], 0]))
-
-# %%
-X, iy, iy2, iy3, iy4, iy5, iy6 = (np.zeros(0) for _ in range(7))
-
-for i in range(slices.shape[0] - 1):
-    iy = np.append(iy, np.mean(temp[slices[i]: slices[i+1]]))
-    iy3 = np.append(iy3, np.mean(temp2[slices[i]: slices[i+1]]))
-    iy5 = np.append(iy5, np.mean(temp3[slices[i]: slices[i+1]]))
-
-    iy2 = np.append(iy2, np.std(main[slices[i]: slices[i + 1], 0]))
-    iy4 = np.append(iy4, np.std(main2[slices[i]: slices[i + 1], 0]))
-    iy6 = np.append(iy6, np.std(main3[slices[i]: slices[i + 1], 0]))
-
-    X = np.append(X, np.mean([MASS_BINS[i], MASS_BINS[i + 1]]))
+    # y2 = np.append(y2, np.std(main[slices[i]: slices[i + 1], 0]))
+    # y4 = np.append(y4, np.std(main2[slices[i]: slices[i + 1], 0]))
+    # y6 = np.append(y6, np.std(main3[slices[i]: slices[i + 1], 0]))
 
 # %%
-plt.style.use(['science'])
+# X, iy, iy2, iy3, iy4, iy5, iy6 = (np.zeros(0) for _ in range(7))
 
-fig = plt.figure(figsize=(5, 5), dpi=150)
+# for i in range(slices.shape[0] - 1):
+#     # iy = np.append(iy, np.mean(temp[slices[i]: slices[i+1]]))
+#     # iy3 = np.append(iy3, np.mean(temp2[slices[i]: slices[i+1]]))
+#     # iy5 = np.append(iy5, np.mean(temp3[slices[i]: slices[i+1]]))
 
-plt.plot(X, Y, '#008BF8', linestyle='--', linewidth=1.5, label='_Jack-knife Gaussian')
-plt.plot(X, y2, '#DC0073', linestyle='--', linewidth=1.5, label='_Intrinsic Gaussian')
+#     # iy2 = np.append(iy2, np.std(main[slices[i]: slices[i + 1], 0]))
+#     # iy4 = np.append(iy4, np.std(main2[slices[i]: slices[i + 1], 0]))
+#     # iy6 = np.append(iy6, np.std(main3[slices[i]: slices[i + 1], 0]))
 
-plt.plot(X, y3, '#008BF8', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz')
-plt.plot(X, y4, '#DC0073', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz')
+#     X = np.append(X, np.mean([MASS_BINS[i], MASS_BINS[i + 1]]))
 
-# plt.plot(X, iy, '#008BF8', linestyle='--', linewidth=1.5, label='_Jack-knife Gaussian Mean')
-# plt.plot(X, iy2, '#DC0073', linestyle='--', linewidth=1.5, label='_Intrinsic Gaussian Mean')
+# # %%
+# plt.style.use(['science'])
 
-# plt.plot(X, iy3, '#008BF8', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz Mean')
-# plt.plot(X, iy4, '#DC0073', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz Mean')
+# fig = plt.figure(figsize=(5, 5), dpi=150)
 
-plt.plot(X, y5, '#008BF8', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute')
-plt.plot(X, y6, '#DC0073', linestyle='-', linewidth=1.5, label='_Intrinsic Absolute')
+# plt.plot(X, Y, '#008BF8', linestyle='--', linewidth=1.5, label='_Jack-knife Gaussian')
+# plt.plot(X, y2, '#DC0073', linestyle='--', linewidth=1.5, label='_Intrinsic Gaussian')
 
-# plt.plot(X, iy5, '#008BF8', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute Mean')
-# plt.plot(X, iy6, '#DC0073', linestyle='-', linewidth=1.5, label='_Intrinsic Absolute Mean')
+# plt.plot(X, y3, '#008BF8', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz')
+# plt.plot(X, y4, '#DC0073', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz')
 
-plt.plot([], [], '--', color='#1E152A', linewidth=1.5, label='Gaussian')
-plt.plot([], [], ':', color='#1E152A', linewidth=1.5, label='Lorentz')
-plt.plot([], [], '-', color='#1E152A', linewidth=1.5, label='Absolute')
-plt.plot([], [], '#008BF8', linewidth=1.5, label='Jack-knife')
-plt.plot([], [], '#DC0073', linewidth=1.5, label='Intrinsic')
+# # plt.plot(X, iy, '#008BF8', linestyle='--', linewidth=1.5, label='_Jack-knife Gaussian Mean')
+# # plt.plot(X, iy2, '#DC0073', linestyle='--', linewidth=1.5, label='_Intrinsic Gaussian Mean')
 
-# plt.ylabel(r'$<\sigma_{\mathrm{jk}}> \mathrm{or} \ \sigma(c)$')
-plt.ylabel(r'$Med(\sigma_{\mathrm{jk}}) \ \mathrm{or} \ \sigma(c)$')
-plt.xlabel(r'$M_{\rm{vir}}$')
-plt.xscale('log')
-plt.legend()
-# plt.show()
-plt.savefig('1.png')
-plt.clf()
+# # plt.plot(X, iy3, '#008BF8', linestyle='dotted', linewidth=1.5, label='_Jack-knife Lorentz Mean')
+# # plt.plot(X, iy4, '#DC0073', linestyle='dotted', linewidth=1.5, label='_Intrinsic Lorentz Mean')
+
+# plt.plot(X, y5, '#008BF8', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute')
+# plt.plot(X, y6, '#DC0073', linestyle='-', linewidth=1.5, label='_Intrinsic Absolute')
+
+# # plt.plot(X, iy5, '#008BF8', linestyle='-', linewidth=1.5, label='_Jack-knife Absolute Mean')
+# # plt.plot(X, iy6, '#DC0073', linestyle='-', linewidth=1.5, label='_Intrinsic Absolute Mean')
+
+# plt.plot([], [], '--', color='#1E152A', linewidth=1.5, label='Gaussian')
+# plt.plot([], [], ':', color='#1E152A', linewidth=1.5, label='Lorentz')
+# plt.plot([], [], '-', color='#1E152A', linewidth=1.5, label='Absolute')
+# plt.plot([], [], '#008BF8', linewidth=1.5, label='Jack-knife')
+# plt.plot([], [], '#DC0073', linewidth=1.5, label='Intrinsic')
+
+# # plt.ylabel(r'$<\sigma_{\mathrm{jk}}> \mathrm{or} \ \sigma(c)$')
+# plt.ylabel(r'$Med(\sigma_{\mathrm{jk}}) \ \mathrm{or} \ \sigma(c)$')
+# plt.xlabel(r'$M_{\rm{vir}}$')
+# plt.xscale('log')
+# plt.legend()
+# # plt.show()
+# plt.savefig('1.png')
+# plt.clf()
 
 # # %%
 # plt.figure(figsize=(20,16), dpi=150)
