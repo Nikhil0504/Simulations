ac8dda580f212d0c9f41b7f114553234d89240df
Nikhil0504 <nikhilgaruda@live.com>
2023-07-19 13:48:07 -0700

    make it more modular for NFW profiles and orbiting profiles

--- Rozo/halos.py
+++ Rozo/halos.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 from typing import Union
 
 import scipy.optimize as so
-from constants import PART_MASS, RADIUS_BINS, VOLUME
-from functions import cost, rho_r
+from constants import PART_MASS, RADIUS_BINS, VOLUME, RADIUS
+from functions import cost, rho_r, cost_nfw
 from imports import iminuit, jit, np
 
 
@@ -15,17 +15,17 @@ class Halo:
     # rps: Union[np.ndarray, float]
 
     def __init__(self, mvir, rvir, rps) -> None:
-        self.mvir = mvir
-        self.rvir = rvir
+        self.m200m = mvir
+        self.r200m = rvir
         self.rps = rps
 
     @property
-    def Mvir(self):
-        return self.mvir
+    def M200m(self):
+        return self.m200m
 
     @property
-    def Rvir(self):
-        return self.rvir
+    def R200m(self):
+        return self.r200m
 
     @jit(fastmath=True, parallel=True, forceobj=True, error_model="numpy")
     def densities(self, factor=1):
@@ -38,31 +38,43 @@ class Halo:
 
     @jit(fastmath=True, parallel=True, forceobj=True, error_model="numpy")
     def NFWs(self, Rs, mask):
-        radii, rhos = rho_r(Rs, self.Mvir, self.Rvir, mask)
+        radii, rhos = rho_r(Rs, self.M200m, self.R200m, mask)
         return radii, rhos
 
-    def minimise_cost(self, Den=[], eps=0.1, cost_func="gaussian", lib='iminuit'):
+    def minimise_cost(self, Den=[], eps=0.1, cost_func="gaussian", lib='iminuit', profile='orbiting'):
         if Den == []:
             den = self.densities()
         else:
             den = Den
 
-        mask = np.where(den > 0)
+        mask = (den > 0) & (RADIUS < self.R200m) & (RADIUS > 0.1)
         den = den[mask]
 
         
         if lib == 'iminuit':
-            optres = iminuit.minimize(cost, [843.8],
-                                    args=(self.Mvir, den, mask, eps, cost_func),
+            if profile == 'orbiting':
+                optres = iminuit.minimize(cost, [843.8],
+                                        args=(self.M200m, den, mask, eps, cost_func),
+                                        method='simplex',
+                                        tol=1e-4,
+                                        options={'stra': 2, 'maxfun': 500})
+                return optres.x[0]
+
+            elif profile == 'NFW':
+                optres = iminuit.minimize(cost_nfw, [np.log(5)],
+                                    args=(self.M200m, den, mask, self.R200m, eps, cost_func),
                                     method='simplex',
+                                    bounds=(np.log(1e-2), np.log(50)),
                                     tol=1e-4,
                                     options={'stra': 2, 'maxfun': 500})
-            return optres.x[0]
+                return optres.x[0]
+            else:
+                raise ValueError("Profile must be either 'orbiting' or 'NFW'")
         
         elif lib == 'scipy':
             optres = so.basinhopping(cost, 843.8, stepsize=1,
             minimizer_kwargs={"method": "Nelder-Mead", 
-                              "args": (self.Mvir, den, mask, eps, cost_func)})
+                              "args": (self.M200m, den, mask, eps, cost_func)})
             return optres.x
         
         return np.inf
